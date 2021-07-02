b287ebab2df9dc4ad4c15b3f161656f598605ff7
MarvinJWendt <git@marvinjwendt.com>
2021-07-03 00:31:40 +0200

    feat(capture): add `Stderr`

--- capture.go
+++ capture.go
@@ -41,3 +41,35 @@ func (h *CaptureHelper) Stdout(capture func(w io.Writer) error) (string, error) 
 
 	return string(out), nil
 }
+
+// CaptureStderr captures everything written to stderr from a specific function.
+// You can use this method in tests, to validate that your functions writes a string to the terminal.
+func (h *CaptureHelper) Stderr(capture func(w io.Writer) error) (string, error) {
+	originalStderr := os.Stderr
+	r, w, err := os.Pipe()
+	if err != nil {
+		return "", fmt.Errorf("could not capture stderr: %w", err)
+	}
+	os.Stderr = w
+
+	err = capture(w)
+	if err != nil {
+		return "", fmt.Errorf("error inside capture function while capturing stderr: %w", err)
+	}
+
+	err = w.Close()
+	if err != nil {
+		return "", fmt.Errorf("could not capture stderr: %w", err)
+	}
+	out, err := ioutil.ReadAll(r)
+	if err != nil {
+		return "", fmt.Errorf("could not capture stderr: %w", err)
+	}
+	os.Stderr = originalStderr
+	err = r.Close()
+	if err != nil {
+		return "", fmt.Errorf("could not capture stderr: %w", err)
+	}
+
+	return string(out), nil
+}
--- capture_test.go
+++ capture_test.go
@@ -3,6 +3,7 @@ package testza
 import (
 	"fmt"
 	"io"
+	"os"
 	"strconv"
 	"testing"
 )
@@ -25,6 +26,30 @@ func TestCaptureHelper_Stdout(t *testing.T) {
 	for i, tt := range tests {
 		t.Run(strconv.Itoa(i), func(t *testing.T) {
 			got, err := Use.Capture.Stdout(tt.args.capture)
+			Use.Assert.Nil(t, err)
+			Use.Assert.Equal(t, got, tt.want)
+		})
+	}
+}
+
+func TestCaptureHelper_Stderr(t *testing.T) {
+	type args struct {
+		capture func(w io.Writer) error
+	}
+	tests := []struct {
+		args args
+		want string
+	}{
+		{args: args{capture: func(w io.Writer) error { fmt.Fprint(os.Stderr, "Hello, World!"); return nil }}, want: "Hello, World!"},
+		{args: args{capture: func(w io.Writer) error { fmt.Fprint(os.Stderr, " Hello, World! "); return nil }}, want: " Hello, World! "},
+		{args: args{capture: func(w io.Writer) error { fmt.Fprint(os.Stderr, "H\ne\nl\nl\nl\no\n\n\n\nWorld!"); return nil }}, want: "H\ne\nl\nl\nl\no\n\n\n\nWorld!"},
+		{args: args{capture: func(w io.Writer) error { fmt.Fprintln(os.Stderr, "Hello, World!"); return nil }}, want: "Hello, World!\n"},
+		{args: args{capture: func(w io.Writer) error { fmt.Fprintln(os.Stderr, "Hello, \nWorld!"); return nil }}, want: "Hello, \nWorld!\n"},
+	}
+
+	for i, tt := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			got, err := Use.Capture.Stderr(tt.args.capture)
 			Use.Assert.Nil(t, err)
 			Use.Assert.Equal(t, got, tt.want)
 		})
